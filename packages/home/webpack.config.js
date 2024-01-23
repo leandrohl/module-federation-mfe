415bf2a206ecb28fffdf472a170541b909e3bb40
“Leandro <leandro.silva.ext@vr.com.br>
2024-01-23 07:50:55 -0300

    feat: configuracao do mfe home

---
+++ packages/home/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, './build'),
+    publicPath: 'http://localhost:3001/'
+  },
+  devServer: {
+    port: 3001,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: [require.resolve("@babel/preset-react")],
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    })
+  ]
+}
